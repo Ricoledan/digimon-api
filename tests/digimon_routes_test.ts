0a03f9cb157823343d00e17de270bc6dba19c17e
Ricardo <ricoledan@gmail.com>
2023-05-21 18:22:31 -0400

    update tests

--- tests/digimon_routes_test.ts
+++ tests/digimon_routes_test.ts
@@ -1,14 +1,20 @@
 import { assertEquals } from "https://deno.land/std@0.109.0/testing/asserts.ts";
 
-Deno.test("Plural Users Route", async () => {
-  const res = await fetch(`http://localhost:3000/digimon`);
+Deno.test("Plural Digimon Route", async () => {
+  const res = await fetch(`http://localhost:3000/api/digimon`);
   const response = await res.json();
   console.log(response);
   assertEquals(res.status, 200);
+  assertEquals(response, [
+    { id: 1, name: "Agumon" },
+    { id: 2, name: "Palmon" },
+    { id: 3, name: "Guilmon" },
+    { id: 4, name: "Renamon" },
+  ]);
 });
 
 Deno.test("Single User Route & User Data", async () => {
-  const res = await fetch(`http://localhost:3000/digimon/2`);
+  const res = await fetch(`http://localhost:3000/api/digimon/2`);
   const response = await res.json();
   assertEquals(res.status, 200);
   assertEquals(response.name, "Palmon");
