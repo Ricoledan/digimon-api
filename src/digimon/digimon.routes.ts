2ab3fa51effb05a81d465c9c6849912dc6f2e861
Ricardo <ricoledan@gmail.com>
2023-05-21 18:23:10 -0400

    terminate logic when id is found

--- src/digimon/digimon.routes.ts
+++ src/digimon/digimon.routes.ts
@@ -2,7 +2,6 @@
 import express, { Request, Response, Router } from "npm:express@4.18.2";
 import demoData from "../../data_blob.json" assert { type: "json" };
 
-
 const router: Router = express.Router();
 
 router.get("/digimon", (_req: Request, res: Response) => {
@@ -11,12 +10,17 @@ router.get("/digimon", (_req: Request, res: Response) => {
 
 router.get("/digimon/:id", (req: Request, res: Response) => {
   const idx: number = Number(req.params.id);
+  let found = false;
   for (const user of demoData.digimon) {
     if (user.id === idx) {
       res.status(200).json(user);
+      found = true;
+      break;
     }
   }
-  res.status(400).json({ msg: "User not found" });
+  if (!found) {
+    res.status(400).json({ msg: "User not found" });
+  }
 });
 
-export default Router;
+export default router;
