902b17fa539c33285df44849b24827975865a7a1
Ricardo Ledan <ricoledan@gmail.com>
2023-05-22 12:22:24 -0400

    update logic

--- src/digimon/digimon.controller.ts
+++ src/digimon/digimon.controller.ts
@@ -1,14 +1,37 @@
 // @deno-types="npm:@types/express@4"
-import { Request, Response} from "npm:express@4.18.2";
+import { Request, Response } from "npm:express@4.18.2";
 import DigimonService from './digimon.service.ts';
+
 class DigimonController {
-    getDigimon(res: Response) {
-        return DigimonService.getDigimon(res);
+    private digimonService: DigimonService;
+
+    constructor() {
+        this.digimonService = new DigimonService();
     }
 
-    getOneDigimon(req: Request, res: Response) {
-        return DigimonService.getOneDigimon(req, res);
+    async getDigimon(res: Response) {
+        try {
+            const digimon = await this.digimonService.getDigimon();
+            return res.status(200).json(digimon);
+        } catch (error) {
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    async getOneDigimon(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const digimon = await this.digimonService.getOneDigimon(id);
+
+            if (digimon) {
+                return res.status(200).json(digimon);
+            } else {
+                return res.status(404).json({ error: 'Digimon not found' });
+            }
+        } catch (error) {
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
 }
 
-export default new DigimonController;
+export default new DigimonController();
--- src/digimon/digimon.service.ts
+++ src/digimon/digimon.service.ts
@@ -1,25 +1,13 @@
-// @deno-types="npm:@types/express@4"
-import { Request, Response} from "npm:express@4.18.2";
 import demoData from "../../data_blob.json" assert { type: "json" };
+
 class DigimonService {
-    async getDigimon(res: Response) {
-        return await res.status(200).json(demoData.digimon);
+    getDigimon() {
+        return demoData.digimon;
     }
 
-    async getOneDigimon(req: Request, res: Response) {
-        const idx = Number(req.params.id);
-        let found = false;
-        for (const digimon of demoData.digimon) {
-            if (digimon.id === idx) {
-                await res.status(200).json(digimon);
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            await res.status(400).json({ msg: "User not found" });
-        }
+    getOneDigimon(id: number) {
+        return demoData.digimon.find((d) => d.id === id) || null;
     }
 }
 
-export default new DigimonService();
+export default DigimonService;
